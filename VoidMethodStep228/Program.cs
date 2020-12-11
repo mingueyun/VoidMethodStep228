3fdcb1169bb9fa38b16b37febd9eb8974f60f4dd
student <student@learncodinganywhere.com>
2020-12-10 18:22:06 -0800

    Add project files.

---
+++ VoidMethodStep228/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodStep228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeatBucket seat = new SeatBucket();
+            seat.Alpha = 4;
+            seat.bravo = 4;
+            seat.Add();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethodStep228/SeatBucket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodStep228
+{
+    class SeatBucket
+    {
+        public int Alpha { get; set; }
+        public int bravo { get; set; }
+
+        public void Add()
+        {
+            int whatNumber = Alpha + bravo;
+            Console.WriteLine("Math Operation : " + whatNumber + "\nDisplaying second int : " + bravo);
+        }
+    }
+}
